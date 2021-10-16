1d2e26a1aa73250efcfdb47e55b999bba611aee5
Boris <b.tseytlin@lambda-it.ru>
2021-10-16 14:35:52 +0300

    Add autotests, refactor code

---
+++ regression_classifier/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn import metrics
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def dataframe_metrics(y_test, y_pred):
+    stats = [
+       metrics.mean_absolute_error(y_test, y_pred),
+       np.sqrt(metrics.mean_squared_error(y_test, y_pred)),
+       metrics.r2_score(y_test, y_pred),
+       mean_absolute_percentage_error(y_test, y_pred)
+    ]
+    return stats
